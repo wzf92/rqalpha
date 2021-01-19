84ed6c60377adc6cef7c7cd3e32a56691e03160f
wzf <wzf_92@163.com>
2021-01-19 09:58:14 +0800

    流量阈值突破策略：局部参数遍历时，回测效果评价指标考虑交易频率

--- tools/SummaryPickle.py
+++ tools/SummaryPickle.py
@@ -2,10 +2,25 @@ import os
 import sys
 import glob
 import pymongo
+import datetime
 import pandas as pd
 
 # 这个路径要和上面的"output_file"对应
 #for name in glob.glob("/root/projects/rqalpha/results/*.pkl"):
+
+def _calc_score(params):
+    dt_start = datetime.datetime.strptime(params['start_date'], '%Y-%m-%d')
+    dt_end = datetime.datetime.strptime(params['end_date'], '%Y-%m-%d')
+    days = (dt_end - dt_start).days
+    trades_per_day = float(params['trades_cnt']) / days
+    if trades_per_day < 0.05:
+        return -1
+    return round(params['total_returns']/params['max_drawdown'], 4) if params['max_drawdown'] > 0.000001 else 0
+
+output_file = None
+if len(sys.argv) > 1:
+    output_file = open(sys.argv[2], 'w')
+    output_file.write('contract_name,backtest_time,bk,sp,sk,bp,stop_profit,stop_loss,score,annualized_returns,total_returns,max_drawdown,sharpe,trades_cnt\n')
 
 for name in glob.glob(sys.argv[1]):
     file_name = os.path.basename(name).replace('.pkl', '')
@@ -41,9 +56,26 @@ for name in glob.glob(sys.argv[1]):
     stop_profit = v[4]
     stop_loss= v[5]
     test_time = summary['start_date'] + '/' + summary['end_date']
+    total_returns = str(summary['total_returns'])
     annualized_returns = str(summary['annualized_returns'])
     max_drawdown = str(summary['max_drawdown'])
-    score = str(round(summary['total_returns'] / summary['max_drawdown'], 4))
+
+    dt_start = datetime.datetime.strptime(summary['start_date'], '%Y-%m-%d')
+    dt_end = datetime.datetime.strptime(summary['end_date'], '%Y-%m-%d')
+    days = (dt_end - dt_start).days
+    trades_per_day = float(len(trades)) / days
+    if trades_per_day < 0.05:
+        score = '-1'
+    else:
+        score = str(round(summary['total_returns'] / summary['max_drawdown'], 4)) if summary['max_drawdown'] > 0 else '-1'
+
     sharpe = str(summary['sharpe'])
     trades_cnt = str(int(len(trades) / 2))
-    print(','.join([contract_name, test_time, score, bk, sp, sk, bp, annualized_returns, max_drawdown, sharpe, stop_profit, stop_loss, trades_cnt]))
+    res_line = ','.join([contract_name, test_time, bk, sp, sk, bp, stop_profit, stop_loss, score, annualized_returns, total_returns, max_drawdown, sharpe, trades_cnt])
+    if output_file:
+        output_file.write('%s\n' % res_line)
+    else:
+        print(res_line)
+
+if output_file:
+    output_file.close()
